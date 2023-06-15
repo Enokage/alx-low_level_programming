d8944e7ca56750a9cdae1cb85825990af25db916
Edekhawe Abraham <Enokageab@gmail.com>
2023-06-15 13:11:35 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
+
 /**
- * main  
- * Description - Printing with the printf function
- * Return: 0 (success)
- */
+ * main - Entry point
+ * Description: A C program that prints with printf function.
+ * Return: 0 (Success)
+*/
+
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
