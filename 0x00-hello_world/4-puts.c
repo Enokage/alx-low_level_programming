e1530baafa5722d26cc55b5714046670b6e28599
Edekhawe Abraham <Enokageab@gmail.com>
2023-06-15 12:39:35 -0700

    the betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /** main-Prints aa program that prints with puts function
- * Return:(success)
+ * Return: 0 (success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
