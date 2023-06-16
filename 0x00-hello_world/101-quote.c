fcd1dfc6d0a1f1c9df96aef06f5eb22a76c7b3a1
Edekhawe Abraham <Enokageab@gmail.com>
2023-06-16 00:35:49 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 /**
  *main - Entry point
  *Description: print a quote using write function
- *Return:  1.
-*/
+ *Return: 1.
+!*/
 int main(void)
 {
 int s;
 s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-write(2,
-"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", s);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", s);
 return (1);
 }
