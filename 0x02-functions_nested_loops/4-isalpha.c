92ca193f4187f9032aeebe7d5fac7c2d9f012678
Edekhawe Abraham <Enokageab@gmail.com>
2023-06-21 14:52:53 -0700

    betty style

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,3 +1,8 @@
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
+ */
 int _isalpha(int c)
 {
     if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
